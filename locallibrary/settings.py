bd9bee614e787da0f64a5dfd79fa580764d7da95
Martin Stefanny Bucag <MBucag@Arcanys.com>
2017-08-16 17:04:40 +0800

    Update ALLOWED_HOSTS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = [
-    'fierce-ridge-66753.herokuapp.com',
+    'djangolocallibrary.herokuapp.com',
     '127.0.0.1',
 ]
 
