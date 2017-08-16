c2d13c9351c70bf0615fcecb92644b5d2890267a
stefbucag <MBucag@Arcanys.com>
2017-08-16 18:27:15 +0800

    Apply pep8 standards for some files Remove unnecessary comments

--- catalog/models.py
+++ catalog/models.py
@@ -11,16 +11,15 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
-    
+
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
-        # return '%s, %s (%s - %s)' % (self.last_name, self.first_name, self.date_of_birth, self.date_of_death)
         author = '{author.last_name}, {author.first_name}'.format(author=self)
-        
+
         return author
 
     class Meta:
--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -7,18 +7,18 @@ class AuthorModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        #Set up non-modified objects used by all test methods
+        # Set up non-modified objects used by all test methods
         Author.objects.create(first_name='Big', last_name='Bob')
 
     def test_first_name_label(self):
-        author=Author.objects.get(id=1)
+        author = Author.objects.get(id=1)
         field_label = author._meta.get_field('first_name').verbose_name
-        self.assertEquals(field_label,'first name')
+        self.assertEquals(field_label, 'first name')
 
     def test_date_of_death_label(self):
-        author=Author.objects.get(id=1)
+        author = Author.objects.get(id=1)
         field_label = author._meta.get_field('date_of_death').verbose_name
-        self.assertEquals(field_label,'Died')
+        self.assertEquals(field_label, 'Died')
 
     def test_first_name_max_length(self):
         author=Author.objects.get(id=1)
